c655044c38430c7d95c752f1f7e76ed0c8c6336d
NexaERP-Software <nexaerphelp@gmail.com>
2026-09-07 00:16:56 +0500

    Create page.js

---
+++ app/page.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: "40px", fontFamily: "Arial" }}>
+      <h1>NexaERP Fast Food</h1>
+      <p>Next.js app is working.</p>
+    </main>
+  );
+}
